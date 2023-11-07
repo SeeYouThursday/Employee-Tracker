5d72756f7db6767c4673ba0fb1570033765bb82e
Brian <galyenb@vcu.edu>
2023-11-07 10:53:56 -0500

    added basic dependencies ERR: no package.json was created with npm i

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-const employee = "";
+const express = require("express");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false })); // do I need to use this?
+app.use(express.json());
+
+// const db = mysql.createConnection(
+//   {
+//     host: "localhost",
+//     // MySQL username,
+//     user: "root",
+//     // MySQL password
+//     password: "",
+//     database: "employee_db",
+//   },
+//   console.log(`Connected to the classlist_db database.`)
+// );
