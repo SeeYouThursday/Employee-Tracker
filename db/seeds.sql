d9e514e7377d686892ef316d4c610158b89c2a00
Brian <galyenb@vcu.edu>
2023-11-07 12:21:33 -0500

    added seeds for each table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,47 @@
--- INSERT INTO
+INSERT INTO DEPARTMENT (
+    NAME
+) VALUES (
+    "Video Juegos"
+),
+(
+    "Being Cereal"
+),
+(
+    "Mom Jokes"
+);
+
+INSERT INTO ROLE (
+    TITLE,
+    SALARY,
+) VALUES (
+    "JOKE CURATOR",
+    40000
+),
+(
+    "Joke Teller",
+    50000
+),
+(
+    "Joke Listener",
+    60000
+),
+(
+    "Therapist for Joke Listener",
+    70000
+);
+
+INSERT INTO EMPLOYEE (
+    FIRST_NAME,
+    LAST_NAME,
+) VALUES (
+    "Brian",
+    "Is-The-Best"
+),
+(
+    "Greg",
+    "Is-The-Worst"
+),
+(
+    "Jared",
+    "Is-The-Okayest"
+);
